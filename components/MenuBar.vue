eaafd3a0989587a46711cfca1d5a141be8005ca8
rm <sirycity2018@gmail.com>
2019-04-07 11:33:32 +0900

    menu調整

--- components/MenuBar.vue
+++ components/MenuBar.vue
@@ -2,21 +2,21 @@
 <template lang="pug">
    nav#menubar__wrapper
      div#menubar
-       div
-         nuxt-link(:to='{name: "index"}'): img(src='~/assets/shibata_logo_white.svg')
-         p ホーム
-       div
-         nuxt-link(:to='{name: "map"}'): img(src='~/assets/menu-icon-map.svg')
-         p ちず
-       div
-         nuxt-link(:to='{name: "stamp"}'): img(src='~/assets/menu-icon-stamp.svg')
-         p スタンプ
-       div
-         nuxt-link(:to='{name: "qr"}'): img(src='~/assets/menu-icon-qr.svg')
-         p QRコード
-       div
-         nuxt-link(:to='{name: "about"}'): img(src='~/assets/shibata_logo_white.svg')
-         p せいさく
+      nuxt-link(:to='{name: "index"}')
+        img(src='~/assets/shibata_logo_white.svg')
+        p ホーム
+      nuxt-link(:to='{name: "map"}')
+        img(src='~/assets/menu-icon-map.svg')
+        p ちず
+      nuxt-link(:to='{name: "stamp"}')
+        img(src='~/assets/menu-icon-stamp.svg')
+        p スタンプ
+      nuxt-link(:to='{name: "qr"}')
+        img(src='~/assets/menu-icon-qr.svg')
+        p QRコード
+      nuxt-link(:to='{name: "about"}')
+        img(src='~/assets/shibata_logo_white.svg')
+        p せいさく
 </template>
 
 
@@ -36,30 +36,21 @@
     display flex
     align-items center
     justify-content space-around
-    div
+    a
       width 25%
       height 100%
       display flex
       flex-direction column
-      justify-content center
+      justify-content space-around
       align-items center
-
-      a
-        display block
-        width 50px
-        height 50px
-        background-color var(--color-darkblue)
-        color var(--color-white)
-        text-decoration none
-        border-radius 50%
-        margin-top -20px
-        display flex
-        justify-content center
-        align-items center
-        img
-          width 64%
-          height 64%
+      text-decoration none
+      color var(--color-white)
+      background-color var(--color-darkblue)
+      img
+        width 50%
+        height 50%
       p
         font-size 14px
+        font-weight bold
 
 </style>
